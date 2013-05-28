11240fd8a81a3d652745f8b262398b8ca6e6303c
Zhang Han <Zhanghan@workmail.com>
2013-05-28 11:01:09 +0800

    done!

--- Gauss.f90
+++ Gauss.f90
@@ -9,23 +9,42 @@ implicit none
 	m = reshape((/2,1,2,5,5,-1,1,8,1,-3,-4,-4/),(/4, 3/))
 
 	i = 1
-!	do i = 1, 4
-		do j = 1, 3
-			maxvalue = 0.
-			if (maxvalue < m(i, j)) then
-				maxvalue = m(i, j)
-				do k = 1, 4
-					temp(k) = m(k, j)
-					m(k, j) = m(1, j)
-					m(1, j) = temp(k)
-				end do
+	maxvalue = m(i, 1)
+	do j = i, 3
+		if (maxvalue < m(i, j)) then
+			maxvalue = m(i, j)
+		end if
+	end do
+		do k = i, 3
+			if (m(i, k) == maxvalue) then
+				temp(:) = m(:, i)
+				m(:, i) = m(:, k)
+				m(:, k) = temp(:)
 			end if
 		end do
-		p1 = m(i, 2) / m(i, 1)
-		p2 = m(i, 3) / m(i, 1)
-		m(:, 2) = m(:, 2) - m(:, 1) * p1
-		m(:, 3) = m(:, 3) - m(:, 1) * p2
-!	end do
+	p1 = m(i, 2) / m(i, 1)
+	p2 = m(i, 3) / m(i, 1)
+	m(:, 2) = m(:, 2) - m(:, 1) * p1
+	m(:, 3) = m(:, 3) - m(:, 1) * p2
+
+	i = 2
+	maxvalue = m(i, 1)
+	do j = i, 3
+		if (maxvalue < m(i, j)) then
+			maxvalue = m(i, j)
+		end if
+	end do
+		do k = i, 3
+			if (m(i, k) == maxvalue) then
+				temp(:) = m(:, i)
+				m(:, i) = m(:, k)
+				m(:, k) = temp(:)
+			end if
+		end do
+	p1 = m(i, 3) / m(i, 2)
+	m(:, 3) = m(:, 3) - m(:, 2) * p1
+
+
 	write(*, *) m
 
 end program Gauss
